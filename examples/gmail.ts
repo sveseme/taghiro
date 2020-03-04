f0a511eec1e91c276d99bf3f9fa11a79210711f1
Stephan Stephan <stephan.schmidt@spacebase.com>
2020-03-04 17:29:24 +0100

    Fix gmail example.

--- examples/gmail.ts
+++ examples/gmail.ts
@@ -1,14 +1,12 @@
-import { EndsWith, endsWith } from '../src/';
-import { MinSize, hasMinSize } from '../src/';
+import { EndsWith, endingWith } from "../src/";
+import { MinSize, hasMinSize } from "../src/";
 
-function gmailDelete(email: string & EndsWith<'@gmail.com'> & MinSize<15>) {}
+function gmailDelete(email: string & EndsWith<"@gmail.com"> & MinSize<15>) {}
 
-function main() {
-  const email = 'stephan.schmidt@gmail.com';
-  // gmailDelete(email); // would not compile
-  if (endsWith(email, '@gmail.com') && hasMinSize(email, 15)) {
-    gmailDelete(email);
-  } else {
-    //
-  }
+const email = "stephan.schmidt@gmail.com";
+// gmailDelete(email); // would not compile
+if (endingWith(email, "@gmail.com") && hasMinSize(email, 15)) {
+  gmailDelete(email);
+} else {
+  //
 }
