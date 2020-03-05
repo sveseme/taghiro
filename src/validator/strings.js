336ec6bf67a184a9e2ab8acf08384fc26312efa9
Stephan Stephan <stephan.schmidt@spacebase.com>
2020-03-05 16:17:41 +0100

    Delete js files.

--- src/validator/strings.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var __1 = require("..");
-var validator_1 = require("./validator");
-function vNotEmpty(value) {
-    return validator_1.validate(value, __1.isNotEmpty);
-}
-exports.vNotEmpty = vNotEmpty;
